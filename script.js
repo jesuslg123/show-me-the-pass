1833bf50a6d90d93c2863fed39c9bc7c8027484c
José Sánchez <josesanxez@gmail.com>
2018-10-24 18:26:26 +0200

    Code improvement
    Change all the functions by the new functions of ES6 Javascript.
    Added two images for the buton

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
-console.log("hi!")
-
 initialize()
 
 function initialize() {
-	var inputs = document.getElementsByTagName('input')
-	for (i = 0; i < inputs.length; i ++) {
-		var input = inputs[i]
-		var type = input.getAttribute("type")
-		if (type == 'password') {
-			processInput(input)
-		}
+	var inputs = document.querySelectorAll('input[type="password"]')
+	for (let input of inputs) {
+		processInput(input)
 	}
-
 }
 
+function getUrlForBrowser(url){
+	if(typeof(chrome) !== 'undefined')
+		return chrome.extension.getURL(url)
+	else if (typeof(browser) !== 'undefined')
+		return browser.extension.getURL(url)
+	else
+		throw "Unrecognized browser"
+}
 
 function processInput (element) {
-	var parent = element.parentElement
-
+	var button = createButton(element)
+	box = createPasswordBox()
+	element.after(box)
+	box.appendChild(element)
+	element.after(button)
+}
+	
+function createButton(element) {
 	var button = document.createElement("Button")
-	button.textContent = "S/H"
-	button.type = "button"
-	button.onclick = function(e) {
+	var element_style = window.getComputedStyle(element)
+	IMG_LOCK = '<img style="height:100%" src="'+getUrlForBrowser('icons/lock.png')+'">'
+    IMG_UNLOCK = '<img style="height:100%" src="'+getUrlForBrowser('icons/unlock.png')+'">'
+	console.log(element_style.paddingTop)
+
+	try {
+		button.innerHTML = IMG_UNLOCK
+	} catch (error) {
+		button.innerHTML = 'U'
+		console.log(error)
+	}
+	
+	button.type = 'button'
+	button.style.background = 'transparent'
+	button.style.height = '100%'
+	button.style.marginLeft = '-40px'
+	button.style.marginTop = element_style.marginTop
+	button.style.border = 'none'
+	button.onclick = function (e) {
 		var type = element.getAttribute("type")
 		if (type == "text") {
 			type = "password"
+			try {
+				button.innerHTML = IMG_UNLOCK
+			} catch (error) {
+				button.innerHTML = 'U'
+				console.log(error)
+			}
 		} else {
 			type = "text"
+			try {
+				button.innerHTML = IMG_LOCK
+			} catch (error) {
+				button.innerHTML = 'U'
+				console.log(error)
+			}
 		}
 		element.setAttribute("type", type)
 	}
-	customizeButton(button, element)
-	parent.appendChild(button)
+	return button
 }
 
-
-function customizeButton(button, element) {
-	var rect = element.getBoundingClientRect()
-	console.log(rect)
-	button.style.position = 'absolute'
-	button.style.bottom = rect.bottom + 'px'
-	button.style.left = rect.width + 'px'
-	button.style.height = rect.height + 'px'
+function createPasswordBox(){
+	var box = document.createElement('div')
+	box.style.display = 'flex'
+	return box
 }
